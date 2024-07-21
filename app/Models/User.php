49f574f2195809c918d7cc3b52942c2794857307
Aimannawal <aone111181@gmail.com>
2024-07-21 15:59:49 +0700

    Database and Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
 }
